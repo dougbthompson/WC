e13a02e1a2f66dce4297a1ae410ef5c5bb14ebe9
dougbthompson <doug.askaar@gmail.com>
2017-09-11 11:21:38 -0700

    20170911 11:21

---
+++ database/atn_all_argos.sql
@@ -0,0 +1,26 @@
+
+drop table if exists atn_all_argos;
+create table atn_all_argos (
+    id               serial               primary key,
+    deployid         integer              null,
+    ptt              integer              null,
+    instr            varchar(8)           null,
+    recordtype       varchar(8)           null,
+    msgcount         integer              null,
+    duplicates       integer              null,
+    corrupt          integer              null,
+    avginterval      varchar(32)          null,
+    mininterval      varchar(32)          null,
+    date             varchar(32)          null,
+    satellite        varchar(8)           null,
+    locationquality  varchar(4)           null,
+    latitude         double precision     null,
+    longitude        double precision     null,
+    latitude2        double precision     null,
+    longitude2       double precision     null,
+    iq               integer              null,
+    duration         varchar(16)          null,
+    frequency        integer              null,
+    power            double precision     null
+);
+
