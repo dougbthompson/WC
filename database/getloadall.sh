92862d422f0cf9e2632af7b8b260616c74027f85
dougbthompson <doug.askaar@gmail.com>
2017-09-18 08:59:08 -0700

    20170918 08:59

--- database/getloadall.sh
+++ database/getloadall.sh
@@ -5,7 +5,7 @@
 # psql -p 5433 -U dougt -w atndb -c "select 1;"
 #   
 # select table_catalog,table_schema,table_name,column_name,ordinal_position,data_type
-#   from information_schema.columns
+#   from information_schema.columns  
 #  where table_catalog = 'atndb' and table_schema = 'biologging' and table_name = 'atn_all';
 #
 # psql -p 5433 -U dougt -w -A -q atndb -c "
