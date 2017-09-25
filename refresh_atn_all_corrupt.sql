6c1c3e85bfb2b6682f817ec1abff4b3930ef8525
dougbthompson <doug.askaar@gmail.com>
2017-09-25 09:10:54 -0700

    20170925 09:10

---
+++ refresh_atn_all_corrupt.sql
@@ -0,0 +1,15 @@
+
+CREATE OR REPLACE FUNCTION refresh_wc_atn_all_corrupt()
+RETURNS integer AS $$
+  
+DECLARE
+    curs1 CURSOR FOR
+    SELECT wc_id, v1, v2, v3 FROM biologging.wc_zip_corrupt;
+
+    v_wc_id    text;
+
+BEGIN
+    return 1;
+END;
+$$ LANGUAGE plpgsql volatile;
+
