a906165205106b42f1f9bc66088c3f5c052b408f
dougbthompson <doug.askaar@gmail.com>
2017-09-25 09:54:11 -0700

    20170925 09:54

--- refresh_atn_all_corrupt.sql
+++ refresh_atn_all_corrupt.sql
@@ -13,6 +13,7 @@ DECLARE
     v_date        text;
     v_satellite   text;
     v_reason      text;
+    v_bytes       integer[];
 
 BEGIN
 
@@ -43,7 +44,7 @@ BEGIN
                    nullif(v41,'')::integer, nullif(v42,'')::integer, nullif(v43,'')::integer, nullif(v44,'')::integer ]
               into v_bytes
               from biologging.wc_zip_corrupt a
-             where a.wc_id  = v_wc_id
+             where a.wc_id       = v_wc_id
                and a.deployid    = v_deployid
                and a.ptt         = v_ptt
                and a.instrument  = v_instrument
@@ -52,20 +53,19 @@ BEGIN
                and a.reason      = v_reason
              limit 1;
 
-            insert into biologging.atn_all_corrupt ( wc_id, deployid, ptt, instrument, date, duplicates, satellite,
+            insert into biologging.atn_all_corrupt (wc_id, deployid, ptt, instrument, date, duplicates, satellite,
                    location_quality, latitude, longitude, reason, possible_timestamp, possible_type, bytes)
 
-            select wc_id, 
-                   v1, v2, v3, v4, nullif(v5,'')::integer, v6, v7, nullif(v8,'')::double precision, v9,
-                   v10, v11, v12, v_bytes
+            select wc_id, v1, v2, v3, v4, nullif(v5,'')::integer, v6, v7, nullif(v8,'')::double precision, v9, v10
+                   v11, v12, v_bytes
               from biologging.wc_zip_corrupt a
              where a.wc_id  = v_wc_id
-                   a.v1     = v_deployid
-                   a.v2     = v_ptt
-                   a.v3     = v_instrument
-                   a.v4     = v_date
-                   a.v6     = v_satellite
-                   a.v10    = v_reason
+               and a.v1     = v_deployid
+               and a.v2     = v_ptt
+               and a.v3     = v_instrument
+               and a.v4     = v_date
+               and a.v6     = v_satellite
+               and a.v10    = v_reason
              limit 1;
 
         end if;
