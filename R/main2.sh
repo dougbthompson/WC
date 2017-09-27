1be344df2c963166c7fc8e4fbfd80d59226f64c8
dougbthompson <doug.askaar@gmail.com>
2017-09-27 09:51:49 -0700

    20170927 09:51

--- R/main2.sh
+++ R/main2.sh
@@ -3,20 +3,28 @@
 #   7618442     24 -rw-rw-r--   1 dougt    dougt       24468 Sep  5 07:10 ./data/59711dd2efec720e0442736a.csv
 export DIR="/home/dougt/wc/wc"
 
+echo "<    Data File ----------------------- - ------------------------------------ - New Size - Old Size [diff>"
+
 for FILE in `ls -1 data/*.csv`
 do
     export STR=`echo ${FILE} | cut -d'/' -f2 | cut -d '.' -f1`
-    export FNEW=`ls -l data/${STR}.csv | sed -e"s/  / /g" | cut -d' ' -f5`
-    export FOLD=`ls -l data.old/${STR}.csv | sed -e"s/  / /g" | cut -d' ' -f5`
 
+    export FNEW=`ls -l data/${STR}.csv | sed -e"s/  / /g" | cut -d' ' -f5`
     typeset -i INEW=${FNEW}
-    typeset -i IOLD=${FOLD}
+
+    if [ -f "data.old/${STR}.csv" ]; then
+        export FOLD=`ls -l data.old/${STR}.csv | sed -e"s/  / /g" | cut -d' ' -f5`
+        typeset -i IOLD=${FOLD}
+    else
+        typeset -i IOLD=0
+    fi
+
     if [ ${FNEW} != ${FOLD} ]; then
         export FLAG="---"
     else
         export FLAG=""
     fi
 
-    printf "< %36s - %36s - %8s - %8s [%s]>\n" ${FILE} ${STR} ${FNEW} ${FOLD} ${FLAG}
+    printf "< %36s - %36s - %8s - %8s [%s]>\n" ${FILE} ${STR} ${INEW} ${IOLD} ${FLAG}
 done
 
