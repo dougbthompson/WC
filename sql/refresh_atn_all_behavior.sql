91ef4598269bc36431237314b4a55acdf4b8c6d9
dougbthompson <doug.askaar@gmail.com>
2017-09-29 14:30:07 -0700

    20170929 14:30

--- sql/refresh_atn_all_behavior.sql
+++ sql/refresh_atn_all_behavior.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE FUNCTION refresh_atn_all_behavior()
+CREATE OR REPLACE FUNCTION refresh_atn_all_behavior(v_which integer)
 RETURNS integer AS $$
   
 DECLARE
@@ -13,8 +13,19 @@ DECLARE
     v_date_start    text;
     v_date_end      text;
 
+    v_shallow       double precision;
+    v_deep          double precision;
+
+    v_number        integer[];
+    v_shape         text[];
+    v_depth_min     integer[];
+    v_depth_max     integer[];
+    v_duration_min  double precision[];
+    v_duration_max  double precision[];
+
 BEGIN
     open curs1;
+
     loop
     fetch curs1 into v_wc_id, v_deployid, v_ptt, v_instrument, v_date_start, v_date_end;
         exit when not found;
@@ -29,14 +40,92 @@ BEGIN
                and a.date_start    = v_date_start
                and a.date_end      = v_date_end)
         then
+            if v_which == 17
+            then
+              select array[ nullif(v10,'')::integer ], nullif(v16,'')::double precision, nullif(v17,'')::double precision
+                into v_number, v_shallow, v_deep
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v11,'')::integer ] into v_shape
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v12,'')::integer ] into v_depth_min
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v13,'')::integer ] into v_depth_max
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v14,'')::double precision ] into v_duration_min
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v15,'')::double precision ] into v_duration_max
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+            else
+              select array[ nullif(v10,'')::integer, nullif(v16,'')::integer, nullif(v22,'')::integer ],
+                     nullif(v28,'')::double precision, nullif(v29,'')::double precision
+                into v_number, v_shallow, v_deep
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v11,'')::integer, nullif(v17,'')::integer, nullif(v23,'')::integer ] into v_shape
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v12,'')::integer, nullif(v18,'')::integer, nullif(v24,'')::integer ] into v_depth_min
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v13,'')::integer, nullif(v19,'')::integer, nullif(v25,'')::integer ] into v_depth_max
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v14,'')::double precision, nullif(v20,'')::double precision,
+                            nullif(v26,'')::double precision ] into v_duration_min
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+
+              select array[ nullif(v15,'')::double precision, nullif(v21,'')::double precision,
+                            nullif(v27,'')::double precision ] into v_duration_max
+                from biologging.wc_zip_behavior a
+               where a.wc_id = v_wc_id and a.v1 = v_deployid and a.v2 = v_ptt and a.v5 = v_instrument
+                 and a.v7 = v_date_start and a.v8 = v_date_end
+               limit 1;
+            end if;
 
             insert into biologging.atn_all_behavior (wc_id, deployid, ptt, depth_sensor, source,
-                   instrument, count, date_start, date_end, what, number, shape, depth_min, depth_max,
-                   duration_min, duration_max, shallow, deep)
+                   instrument, count, date_start, date_end, what, shallow, deep,
+                   number, shape, depth_min, depth_max, duration_min, duration_max)
 
-            select wc_id, v1, v2, nullif(v3,'')::double precision, v4, v5, v6, v7, v8, v9,
-                   nullif(v10,'')::integer, v11, nullif(v12,'')::integer, nullif(v13,'')::integer,
-                   v14, v15, nullif(v16,'')::double precision, nullif(v17,'')::double precision
+            select wc_id, v1, v2, v3, v4, v5, v6, v7, v8, v9, v_shallow, v_deep,
+                   v_number, v_shape, v_depth_min, v_depth_max, v_duration_min, v_duration_max
 
               from biologging.wc_zip_behavior a
              where a.wc_id  = v_wc_id
