584a7336797333d266c89de53d2f9052b6c0a6da
dougbthompson <doug.askaar@gmail.com>
2017-11-10 09:22:41 -0800

    20171110 09:22

---
+++ sql/acoustic_atn.sql
@@ -0,0 +1,16 @@
+
+drop view if exists acoustic_atn;
+create or replace view acoustic_atn
+as
+
+  select a.code        as code,
+         count(a.code) as detections,
+         a.site        as site,
+         a.station     as station,
+         a.longitude   as longitude,
+         a.latitude    as latitude,
+         a.commoname   as commonname
+
+    from acoustic_data a
+
+
