a0067df011254b8f69b507db63870a0948693200
dougbthompson <doug.askaar@gmail.com>
2017-11-30 10:50:08 -0800

    20171130 10:50

--- sql/acoustic_atn_func.sql
+++ sql/acoustic_atn_func.sql
@@ -50,3 +50,32 @@ RETURNS SETOF acoustic_atn_station AS $$
 
 $$ LANGUAGE sql;
 
+CREATE OR REPLACE FUNCTION acoustic_atn_func_which (p_start_date text, p_end_date text,
+                  commonname text default '%', station_project text default '%', station_region text default '%')
+RETURNS SETOF acoustic_atn_station AS $$
+
+    select ad.code           as code,
+           count(ad.code)    as detections,
+           ast.station_site  as site,
+           ast.station_name  as station,
+           round(cast(avg(ast.longitude) as numeric),4) as longitude,
+           round(cast(avg(ast.latitude)  as numeric),4) as latitude,
+           am.commonname     as commonname
+      from atn_acoustic_data ad,
+           atn_acoustic_station ast,
+           atn_acoustic_meta am
+     where ad.receiver          = ast.receiver
+       and ad.receiver_dnum     = ast.receiver_dnum
+       and ad.ping_detection    > $1::date
+       and ad.ping_detection   <= $2::date
+       and ad.false_hit         = 0
+       and am.ptt               = ad.code
+       and am.commonname       <> ''
+       and am.commonname       like $3::text
+       and ast.station_project like $4::text
+       and ast.station_region  like $5::text
+     group by 1,3,4,7
+     order by 3,4,1;
+
+$$ LANGUAGE sql;
+
