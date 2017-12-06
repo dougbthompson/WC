c21c67e23a79a030cd400eec1db5529bd3521613
dougbthompson <doug.askaar@gmail.com>
2017-12-06 13:44:30 -0800

    20171206 13:44

--- sql/acoustic_atn_func.sql
+++ sql/acoustic_atn_func.sql
@@ -118,6 +118,29 @@ select am.eventid         as eventid,
    and am.commonname       <> ''
  order by ad.code, ad.ping_detection;
 
+-----
+
+select 'White Shark',
+       '2017-12-06',
+       '30',
+       am.eventid         as eventid,
+       ast.station_region as site,
+       ad.ping_detection  as date,
+       ad.code            as code,
+       am.commonname      as commonname
+  from atn_acoustic_data ad,
+       atn_acoustic_station ast,
+       atn_acoustic_meta am
+ where ast.station_site     = 'Palmyra'
+   and ad.receiver          = ast.receiver
+   and ad.receiver_dnum     = ast.receiver_dnum
+   and ad.ping_detection    > 'now'::date - (30 * 1)
+   and ad.ping_detection   <= 'now'::date
+   and ad.false_hit         = 0
+   and am.ptt               = ad.code
+   and am.commonname       <> ''
+ order by ad.code, ad.ping_detection;
+
 
 
 
