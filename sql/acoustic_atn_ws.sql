44518cca3f4b0fd0e07c7bfd838224a4b2dbd420
dougbthompson <doug.askaar@gmail.com>
2017-12-06 14:05:07 -0800

    20171206 14:05

--- sql/acoustic_atn_ws.sql
+++ sql/acoustic_atn_ws.sql
@@ -3,14 +3,14 @@ CREATE OR REPLACE FUNCTION acoustic_atn_plot_ws (p_start_date text, p_end_date t
                   commonname text default '%', station_site text default '%', which_dt_period text default '30')
 RETURNS SETOF atn_plots AS $$
 
-    select am.commonname            as species,
-           now()                    as date_value,
-           which_dt_period          as time_period,
-           am.eventid::varchar(64)  as eventid,
-           ast.station_site         as site,
-           ad.ping_detection        as date_value,
-           ad.code                  as code,
-           am.commonname            as commonname
+    select am.commonname                  as species,
+           now()                          as date_value,
+           which_dt_period                as time_period,
+           am.eventid::varchar(64)        as eventid,
+           ast.station_site               as site,
+           ad.ping_detection::varchar(64) as date_value,
+           ad.code::varchar(64)           as code,
+           am.commonname                  as commonname
       from atn_acoustic_data ad,
            atn_acoustic_station ast,
            atn_acoustic_meta am
